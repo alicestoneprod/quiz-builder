64d937f57ef45091ca78e9e5becd3cbd50bdf7c0
Alice Morozova <alicemorozovafront@gmail.com>
2024-03-18 21:30:22 +0300

    conditionally header render

--- src/features/Layout/Header/Header.tsx
+++ src/features/Layout/Header/Header.tsx
@@ -1,21 +1,54 @@
 import { FC } from "react"
-import { Button, Link } from "components"
+import { Button, Dropdown, Link } from "components"
 import { LinksRow } from "./LinksRow"
+import UserIcon from "assets/icons/profile-icon.svg?react"
 import s from "./Header.module.scss"
+import { useAppSelector } from "shared/hooks"
 
 interface HeaderI {}
 
-export const Header: FC<HeaderI> = ({}) => (
-  <div className={s.header}>
-    <div className={s.titleLinks}>
-      <div className={s.title} />
-      <div className={s.linksBtns}>
-        <LinksRow className={s.linksRow} />
-        <Link to='/login' variant='button'>
-          <Button className={s.loginBtn} variant='outlined'>Login</Button>
-        </Link>
+export const Header: FC<HeaderI> = ({}) => {
+  const user = useAppSelector((state) => state.user)
+
+  if (user?.isAuth) {
+    return (
+      <div className={s.header}>
+        <div className={s.titleLinks}>
+          <div className={s.title} />
+          <div className={s.linksBtns}>
+            <LinksRow className={s.linksRow} />
+            <div className={s.dropdownCnt}>
+              <Dropdown
+                icon={<UserIcon />}
+                iconPosition='left'
+                label={user.user.email.split("@gmail.com")[0]}
+                options={[{ label: "Dashboard", value: "/dashboard" }]}
+                variant='navigate'
+              />
+            </div>
+          </div>
+        </div>
+        <div className={s.line} />
       </div>
-    </div>
-    <div className={s.line} />
-  </div>
-)
+    )
+  }
+
+  if (!user?.isAuth) {
+    return (
+      <div className={s.header}>
+        <div className={s.titleLinks}>
+          <div className={s.title} />
+          <div className={s.linksBtns}>
+            <LinksRow className={s.linksRow} />
+            <Link to='/login' variant='button'>
+              <Button className={s.loginBtn} variant='outlined'>
+                Login
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className={s.line} />
+      </div>
+    )
+  }
+}
