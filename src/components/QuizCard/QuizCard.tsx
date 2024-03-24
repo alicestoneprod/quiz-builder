99ccd19051e22db1e9d3efe455a23d21743c2574
Alice Morozova <alicemorozovafront@gmail.com>
2024-03-24 14:58:25 +0300

    fix quiz img, fix header size

--- src/components/QuizCard/QuizCard.tsx
+++ src/components/QuizCard/QuizCard.tsx
@@ -7,11 +7,7 @@ interface QuizCardI {}
 export const QuizCard: FC<QuizCardI> = ({}) => {
   return (
     <div className={s.quizCard}>
-      <img
-        src='http://localhost/assets/quiz/12412412477182487124.jpg'
-        alt='quiz-card-icon'
-        className={s.icon}
-      />
+      <img src='/assets/card/mock.jpg' alt='quiz-card-icon' className={s.icon} />
       <div className={s.labelsList}>
         <CardLabel>N вопросов</CardLabel>
         <CardLabel>4/5 оценка</CardLabel>
